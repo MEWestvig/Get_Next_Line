ea3152b412d8935cf65181fc6b6fb946db1a59c0
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-05-30 12:28:56 +0200

    Updating gitignore and adding some error checks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mwestvig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 15:42:21 by mwestvig          #+#    #+#             */
-/*   Updated: 2018/05/29 11:34:25 by mwestvig         ###   ########.fr       */
+/*   Updated: 2018/05/30 12:15:38 by mwestvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,20 @@ ssize_t read(int fd, void *buf, size_t count);
 int get_next_line(const int fd, char **line)
 {
 	ssize_t	num_read;
-	void	*buffer;
+	char	buffer[BUFF_SIZE + 1];
 	int		i;
 
-	buffer = (void *)malloc(BUFF_SIZE + 1);
+	if (fd < 0 || !line || read(fd, buffer, 0) < 0)
+		return (-1);
 	num_read = read(fd, buffer, BUFF_SIZE);
+	if (num_read == 0)
+		return (0);
+	//deal with thingy.
 	while (i < (int)num_read)
 	{
 	}
 	//take in the buffer of BUFF_SIZE and go until it hits /n or /0
-	//set each line as the next part of the double array "line"
+	//set the line into "line"
+	//return whether the line could be read or not.
+	//1 for line read, 0 for reading complete, -1 for error.
 }
