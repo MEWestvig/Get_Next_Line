bc7258583f0b0d6723e851e545da0a7ce69d023c
Meghan WESTVIG <mwestvig@c2r5s5.wethinkcode.co.za>
2018-06-04 16:56:22 +0200

    Reset BUFF_SIZE to 32

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mwestvig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 15:42:40 by mwestvig          #+#    #+#             */
-/*   Updated: 2018/06/04 16:24:22 by mwestvig         ###   ########.fr       */
+/*   Updated: 2018/06/04 16:55:45 by mwestvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 32
 
 # include "libft.h"
 
